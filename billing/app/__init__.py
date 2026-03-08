1c10737b303b34f61dbd93af354153270e0dd936
roydann <royid123@gmail.com>
2026-03-08 10:47:38 +0200

    hotfix docker-compose, /truck post method

--- billing/app/__init__.py
+++ billing/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from app.routes.health import health_bp
+from app.routes.truck import truck_bp
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(health_bp)
+    app.register_blueprint(truck_bp)
     return app
